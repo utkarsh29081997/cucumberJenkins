dfdbb653d2c379d7b68e21d1e5a120ac15572468
utkarsh29081997 <38398744+utkarsh29081997@users.noreply.github.com>
2024-02-02 00:23:36 +0530

    Update TestRunner.java

--- opencart_cucumber_framework/src/test/java/testRunner/TestRunner.java
+++ opencart_cucumber_framework/src/test/java/testRunner/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 					//features= {".//Features/"},
 					//features= {".//Features/Login.feature"},
 					//features= {".//Features/LoginDDTExcel.feature"},
-					features= {".//Features/ForgotPassword.feature"},
+					features= {""},
 					//features= {".//Features/Registration.feature"},
 					
 					//features= {"@target/rerun.txt"},
